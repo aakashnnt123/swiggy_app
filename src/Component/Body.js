9d8224b635ed770cbb998d85438950d10d9d25f3
Aakash Sharma <aakash5427896@gmail.com>
2024-06-23 23:27:02 +0530

    commit

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,37 +1,33 @@
 import RestaurantCard from './restCard';
 import './Body.css';
-// import { useState } from 'react';
-import resList from '../Util/Mock_data';
+import { useState } from 'react';
+import resList from '../Util/Mock_data'; 
 
 
 const Body= () => {
 
-// const[ listofRestaurants , setListofRestraunt] = useState([resList]);
+const [ listofRestaurants , filter_list] = useState(resList);
 
 return (
       <div className="body">
         <div className="search">search</div>
         <div className="filter">
           <button 
-               className="filter-btn" 
-               onClick={()=>{
-               
-            //  const f_list = listofRestaurants.filter(
-            //   (res) => res.info.avgRatingString > 4
-              
-            //  );
-            //  setListofRestraunt(f_list);
-             
-          }}
-          
-          >
+               className="filter-btn"   
+               onClick={ ()=> {
+               console.log("button click")    
+             const fil_list = listofRestaurants.filter(
+              (res) => res.info.avgRatingString > 4);
+
+             filter_list(fil_list);
+           }}>
             Top rated restaurant
           </button>
         </div>
         <div className="res_container">
          
           {
-           resList.map((restaurants) => <RestaurantCard key={restaurants.info.id} resData={restaurants}/>)
+           listofRestaurants.map((restaurants) => <RestaurantCard key={restaurants.info.id} resData={restaurants}/>)
           }
         </div>
       </div>
