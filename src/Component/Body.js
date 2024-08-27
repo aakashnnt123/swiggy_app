547b8ac502bdd008ba12fcde391a02d4ad923f27
Aakash Sharma <aakash5427896@gmail.com>
2024-08-28 00:21:18 +0530

    COMMIT

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -13,24 +13,25 @@ const Body = () => {
   // if no depenndency array => use Effect is called on every render
   // if dependency array is empty = [] => useEffect is called on initial render(just once)
   // if dependency array is not-empty = [...] => useEffect is called only when dependency changes.
-   
+  
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.73390&lng=76.78890&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7630356&lng=76.6528225&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.73390&lng=76.78890&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
   
     const json = await data.json();
     console.log(json);
 
     setlistofRestaurants(
-      json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle
+      json?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
-    setfilterrestaurant(json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle
+    setfilterrestaurant(json?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle
       ?.restaurants);
     
   };
