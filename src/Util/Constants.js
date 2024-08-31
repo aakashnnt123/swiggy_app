b2cf6b0b5e0d8c444d884e60052134766c4cb454
Aakash Sharma <aakash5427896@gmail.com>
2024-08-31 17:47:41 +0530

    commit

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -2,7 +2,7 @@ export const CDN_URL=
 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL=
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6cvlaBJ9M3nC0xQm2NQVB1SoYvu8fqK-bUw&s";
+"https://i.pinimg.com/originals/19/a9/65/19a9657e4490b4c75ea5065f78e97cbe.jpg";
 
 export const menu_api=
 "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7630356&lng=76.6528225&restaurantId="
